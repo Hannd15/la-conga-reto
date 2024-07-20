666684ea1d541cf768f6242ababf88909edd135d
Hannd15 <lejhernandez08gmail.com>
2024-07-20 09:03:39 -0500

    Filtro por zonas protegidas Por algúna razón solo se muestra la información de los puntos azules

--- README.md
+++
@@ -1,1 +0,0 @@
-# la-conga-reto
--- main.py
+++ main.py
@@ -1,73 +1,16 @@
 from flask import Flask, render_template
 import pandas as pd
 import folium
+import geopandas as gpd
+from shapely.geometry import Point
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    try:
-        file_path = 'data/Ramsar_sites.csv'
-        df = pd.read_csv(file_path, delimiter=',', quotechar='"')
-
-        #Definción de elementos
-        df['Latitude'] = df['Latitude'].apply(lambda x: dms_to_dd(x))
-        df['Longitude'] = df['Longitude'].apply(lambda x: dms_to_dd(x))
-        df = df.dropna(subset=['Latitude', 'Longitude'])
-        df_colombia = df[df['Country'] == 'Colombia']
-        m = folium.Map(location=[4.5, -74.0], zoom_start=6)
-
-        #Crea capa de estilo
-        folium.TileLayer(
-            tiles='https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png',
-            attr='© OpenStreetMap contributors © CartoDB',
-            name='CartoDB Positron'
-        ).add_to(m)
-
-        #Crea capa de elementos
-        wetland_layer = folium.FeatureGroup(name='Humedales Colombianos')
-
-        def get_color(name):
-            if "Complejo" in name:
-                return 'blue'
-            elif "Sistema" in name:
-                return 'green'
-            else:
-                return 'red'
-
-        #Crea los marcadores de cada humedal
-        for _, row in df_colombia.iterrows():
-            color = get_color(row['Site name'])
-            popup_info = f"""
-            <b>{row['Site name']}</b><br>
-            <button onclick="parent.showWetlandInfo('{row['Site name']}', '{row['Territory']}', '{row['Designation date']}', '{row['Area (ha)']}', '{row['Annotated summary']}', '{row['Latitude']}', '{row['Longitude']}')">Ver Información</button>
-            """
-            folium.Marker(
-                location=[row['Latitude'], row['Longitude']],
-                popup=folium.Popup(popup_info, max_width=225),
-                icon=folium.Icon(color=color)
-            ).add_to(wetland_layer)
-
-        # Añade pas de elementos y estilo
-        wetland_layer.add_to(m)
-        folium.LayerControl().add_to(m)
-        # Crea map.html
-        map_path = 'static/map.html'
-        m.save(map_path)
-
-        # Perpara los datos a pasar
-        wetlands_info = df_colombia.to_dict(orient='records')
-
-    except Exception as e:
-        return f"Error: {e}"
-
-    return render_template('index.html', wetlands_info=wetlands_info)
-
+# Función para convertir coordenadas DMS a decimales
 def dms_to_dd(dms):
     try:
         if isinstance(dms, float):
             return dms
-        #Pasa las coordenadas geograficas a cartecianas
         if isinstance(dms, str):
             dms = dms.replace('°', ' ').replace('\'', ' ').replace('\"', ' ').split()
             if len(dms) < 3:
@@ -84,6 +27,91 @@ def dms_to_dd(dms):
             raise ValueError(f"El valor '{dms}' no es un string válido para la conversión.")
     except Exception as e:
         raise ValueError(f"Error en la conversión de DMS a decimal: {e}")
+
+# Ruta principal
+@app.route('/')
+def home():
+    try:
+        # Carga de datos
+        file_path = 'data/Ramsar_sites.csv'
+        df = pd.read_csv(file_path, delimiter=',', quotechar='"')
+        df['Latitude'] = df['Latitude'].apply(lambda x: dms_to_dd(x))
+        df['Longitude'] = df['Longitude'].apply(lambda x: dms_to_dd(x))
+        df = df.dropna(subset=['Latitude', 'Longitude'])
+        df_colombia = df[df['Country'] == 'Colombia']
+
+        # Carga de zonas de protección ambiental
+        proteccion_ambiental_path = 'data/Parques_Nacionales_Naturales_de_Colombia.shp'
+        gdf_national_parks = gpd.read_file(proteccion_ambiental_path)
+
+        # Conversión a GeoDataFrame
+        gdf_wetlands = gpd.GeoDataFrame(df_colombia, geometry=gpd.points_from_xy(df_colombia.Longitude, df_colombia.Latitude))
+
+        # Intersección espacial
+        protected_wetlands = gpd.sjoin(gdf_wetlands, gdf_national_parks, how='inner', predicate='intersects')
+        
+        # Encuentra los humedales fuera de zonas protegidas
+        unprotected_wetlands = gdf_wetlands[~gdf_wetlands.index.isin(protected_wetlands.index)]
+
+        # Mapa
+        m = folium.Map(location=[4.5, -74.0], zoom_start=6)
+        folium.TileLayer(tiles='https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', attr='© OpenStreetMap contributors © CartoDB', name='CartoDB Positron').add_to(m)
+
+        # Capa de humedales
+        protected_layer = folium.FeatureGroup(name='Humedales en zonas de protección ambiental')
+        not_protected_layer = folium.FeatureGroup(name='Humedales fuera de zonas de protección ambiental')
+
+        # Función para definir color de los marcadores
+        def get_color(name):
+            if "Complejo" in name:
+                return 'blue'
+            elif "Sistema" in name:
+                return 'green'
+            else:
+                return 'red'
+
+        # Marcadores protegidos
+        for _, row in protected_wetlands.iterrows():
+            color = get_color(row['Site name'])
+            popup_info = f"""
+            <b>{row['Site name']}</b><br>
+            <button onclick="parent.showWetlandInfo('{row['Site name']}', '{row['Territory']}', '{row['Designation date']}', '{row['Area (ha)']}', '{row['Annotated summary']}', '{row['Latitude']}', '{row['Longitude']}')">Ver Información</button>
+            """
+            folium.Marker(
+                location=[row['Latitude'], row['Longitude']],
+                popup=folium.Popup(popup_info, max_width=225),
+                icon=folium.Icon(color=color)
+            ).add_to(protected_layer)
+
+        # Marcadores no protegidos
+        for _, row in unprotected_wetlands.iterrows():
+            color = get_color(row['Site name'])
+            popup_info = f"""
+            <b>{row['Site name']}</b><br>
+            <button onclick="parent.showWetlandInfo('{row['Site name']}', '{row['Territory']}', '{row['Designation date']}', '{row['Area (ha)']}', '{row['Annotated summary']}', '{row['Latitude']}', '{row['Longitude']}')">Ver Información</button>
+            """
+            folium.Marker(
+                location=[row['Latitude'], row['Longitude']],
+                popup=folium.Popup(popup_info, max_width=225),
+                icon=folium.Icon(color=color)
+            ).add_to(not_protected_layer)
+
+        # Añade capa de elementos y estilo
+        protected_layer.add_to(m)
+        not_protected_layer.add_to(m)
+        folium.LayerControl().add_to(m)
+
+        # Guarda el mapa
+        map_path = 'static/map.html'
+        m.save(map_path)
+
+        # Prepara los datos a pasar
+        wetlands_info = df_colombia.to_dict(orient='records')
+
+    except Exception as e:
+        return f"Error: {e}"
+
+    return render_template('index.html', wetlands_info=wetlands_info)
 
 if __name__ == '__main__':
     app.run(debug=True)
